f8e0e655bdb538a099a97bbb814f15cd647214d0
solmee <kii5541@naver.com>
2024-02-24 19:53:15 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,44 +7,41 @@ const port = process.env.PORT || 4200;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to database.');
+});
+
 const cors = require('cors'); 
 app.use(cors());
 
-// 서버 테스트용 api
-// app.get('/api/hello', (req, res) => {  
-//     res.send({ message: 'Hello Express!' });
-// });
-
 app.get('/api/customers', (req, res) => {
-    setTimeout(()=>{
-        res.send([
-            {
-              'id': '1',
-              'image': 'https://placeimg.com/64/64/1',
-              'name': '배켜니',
-              'birthday': '961122',
-              'gender': '남자',
-              'job': '대학생'
-            },
-            {
-              'id': '2',
-              'image': 'https://placeimg.com/64/64/2',
-              'name': '소희',
-              'birthday': '961122',
-              'gender': '남자',
-              'job': '대학생'
-            },
-            {
-              'id': '3',
-              'image': 'https://placeimg.com/64/64/3',
-              'name': '은석이',
-              'birthday': '961122',
-              'gender': '남자',
-              'job': '대학생'
+    connection.query(
+        "SELECT * FROM CUSTOMER", // 데이터 가져오는 쿼리문
+        (err, rows, fields) => {
+            if (err) {
+                console.error('Error querying database:', err);
+                res.status(500).send('Error querying database');
+                return;
             }
-          ])
-    }, 5000);
-    
+            res.send(rows);
+        }
+    )
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
